3e71bac6938ed55049e4946207142c8c1ecdcbdb
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2014-07-11 16:51:21 +0000

    Aenderungen übernehmen

--- scripte/update.php
+++ scripte/update.php
@@ -3,13 +3,14 @@
     system('git pull');
     
     // Changelog schreiben 
-    ob_start();
+    //ob_start();
     system('git log --no-merges --date=iso8601 --pretty=format:"%ad : %s (%an)"');
     $tmp_text=ob_get_contents();
     ob_end_clean();
     $tmp_file=fopen("../README","w");
     fwrite($tmp_file,"PHP_Bugiad.de\n");
     fwrite($tmp_file,"=============\n");
+    fwrite($tmp_file,"test\n");
     fwrite($tmp_file,$tmp_text);
     fclose($tmp_file);
     
