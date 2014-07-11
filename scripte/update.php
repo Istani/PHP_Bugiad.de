ae8a8979040ffc7e625472e243915dae5a955d22
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2014-07-11 16:49:31 +0000

    Aenderungen übernehmen

--- scripte/update.php
+++ scripte/update.php
@@ -7,10 +7,10 @@
     system('git log --no-merges --date=iso8601 --pretty=format:"%ad : %s (%an)"');
     $tmp_text=ob_get_contents();
     ob_end_clean();
-    fopen("../README","w");
-    fwrite("PHP_Bugiad.de\n");
-    fwrite("=============\n");
-    fwrite($tmp_text);
+    $tmp_file=fopen("../README","w");
+    fwrite("PHP_Bugiad.de\n",$tmp_file);
+    fwrite("=============\n",$tmp_file);
+    fwrite($tmp_text,$tmp_file);
     fclose();
     
     system('git add .');
