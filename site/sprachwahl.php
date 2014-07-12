9646d117ed8c662899d940c016719f3ed0e637cc
Sascha Kaufamnn <sascha.u.kaufmann@googlemail.com>
2014-07-12 11:40:57 +0200

    Bugfix: Sprachenwahl

--- site/sprachwahl.php
+++ site/sprachwahl.php
@@ -1,8 +1,8 @@
 <?php
     $sprachen_array=$database->sql_select("sta_texte", "*", "true=false",true);
-    unset($sprachen_array['art']);
-    unset($sprachen_array['lfd']);
-    foreach ($sprachen_array as $key=>$value) {
+    unset($sprachen_array[0]["art"]);
+    unset($sprachen_array[0]["lfd"]);
+    foreach ($sprachen_array[0] as $key=>$value) {
         echo '<a href="?sprache='.$key.'"><img src="img/flaggen/'.$key.'.png"></a>&nbsp;';
     }
 ?>
