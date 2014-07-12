554d8b580f26ef180e8face5302b7e8181522838
Sascha Kaufamnn <sascha.u.kaufmann@googlemail.com>
2014-07-12 13:28:34 +0200

    Aenderungen uebernehmen

--- update.php
+++ update.php
@@ -1,23 +1,22 @@
 <?php
+
     // Komplettes Update
-    
-    // Neue Git Daten hohlen 
+    // Neue Git Daten hohlen
     system('git pull');
-    
-    // Changelog schreiben 
+
+    // Changelog schreiben
     ob_start();
     system('git log --no-merges --date=iso8601 --pretty=format:"%ad : %s (%an)"');
-    $tmp_text=ob_get_contents();
+    $tmp_text = ob_get_contents();
     ob_end_clean();
-    $tmp_file=fopen("README","w");
-    fwrite($tmp_file,"PHP_Bugiad.de\n");
-    fwrite($tmp_file,"=============\n");
-    fwrite($tmp_file,"\n");
-    fwrite($tmp_file,$tmp_text);
+    $tmp_file = fopen("README", "w");
+    fwrite($tmp_file, "PHP_Bugiad.de\n");
+    fwrite($tmp_file, "=============\n");
+    fwrite($tmp_file, "\n");
+    fwrite($tmp_file, $tmp_text);
     fclose($tmp_file);
     // Changelog Ã„nderungen hochladen um eine Saubere Version zu haben?
     system('git add .');
     system('git commit -am "Aenderungen uebernehmen"');
-    system('git push');
-    
+    //system('git push'); //Klappt bei mir im Windows nicht, später nach schauen!
 ?>
